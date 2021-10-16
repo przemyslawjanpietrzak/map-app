dea8d1f912819ca7ced008aeef497e5c09aa6d39
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-10-16 20:41:10 +0200

    wip

--- frontend/src/services/countryState.service.ts
+++ frontend/src/services/countryState.service.ts
@@ -12,7 +12,17 @@ export class CountryStateService {
             'Access-Control-Allow-Origin': '*',
         }});
 
-        return data;
+        return this.parseCountryStates(data);
+    }
+
+    private parseCountryStates(countryStates: Array<CountryState>): Array<CountryState> {
+        const countryStatesIds = new Set(countryStates.map(({ id }) => id));
+
+       return Array.from(countryStatesIds).map(countryStateId =>
+            countryStates
+            .filter(({ id }) => id === countryStateId)
+            .reduce((acc, countryState) => ({ ...countryState, visits: acc.visits + countryState.visits}) , { visits: 0, name: null, id: null } as unknown as CountryState)            
+        );
     }
 }
 
--- frontend/src/views/CountryMap.component.tsx
+++ frontend/src/views/CountryMap.component.tsx
@@ -6,11 +6,23 @@ export const CountryMapComponent = (props: CountryMapProps) => {
   if (!props.isCountryStatesLoaded) {
     props.fetchCountryState();
   }
+
+  const choises = [
+    { min: 0, max: 250 },
+    { min: 250, max: 500 },
+    { min: 500, max: 1000 },
+  ] as Array<{ min: number, max: number }>;
   
   return <div>
     { props.isCountryStatesLoaded && <CountryMap visitedCountryStateDict={props.filterdVisitedCountryStateDict} /> }
     <div >
-        <input type="radio" value="0" name="selectedVisited" onClick={() => props.setSelectedVisitedCount({ min: 0, max: 250 })} /> 0 - 250
+        { choises.map({ min, max }, index) => <div key={index}>
+            <input type="radio" value={min} name="selectedVisited" onClick={() => props.setSelectedVisitedCount({ min, max })} />
+            <span>{min} - {max}</span>
+          </div>
+          );
+        }
+     
         <input type="radio" value="250" name="selectedVisited" onClick={() => props.setSelectedVisitedCount({ min: 250, max: 500 })} /> 250 - 500
         <input type="radio" value="500" name="selectedVisited" onClick={() => props.setSelectedVisitedCount({ min: 500, max: 1000 })}/> 500 - 1000
         <input type="radio" value="1000" name="selectedVisited" onClick={() => props.setSelectedVisitedCount({ min: 1000, max: Infinity })}/> 1000+
